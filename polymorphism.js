6dfd0b7a749fccd67cc4e9f786e05ecfbebaaf18
Bandana Shukla <bshukla@bebotechnologies.com>
2022-09-22 01:14:09 +0530

    test git

---
+++ polymorphism.js
@@ -0,0 +1,21 @@
+console.log("Polymorphism in JavaScript");
+
+class Animal {
+    constructor(name)
+    {
+       this.name = name;
+    }
+    eat(){
+        console.log (this.name+' eats food');
+    }
+}
+
+class Alligator extends Animal {
+    eat(){
+        console.log(this.name+' eats fishes');
+
+    }
+    
+    }
+let Shark = new Alligator ('Shark');
+Shark.eat();
